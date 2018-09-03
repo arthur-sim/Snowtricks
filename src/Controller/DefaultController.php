c75708dc11539fa1f9903ae613008cb255b3bb57
Unknown <arthur.simonian@outlook.fr>
2018-09-03 21:47:25 +0200

    display of Upload
    without relation with tricks

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -5,15 +5,33 @@ namespace App\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use App\Entity\Image;
+use App\Form\ImageType;
  
 class DefaultController extends Controller
 {
     /**
      * @Route("/", name="index")
      */
-    public function index()
+    public function index(Request $request)
     {
-        return $this->render('index.html.twig');
+        $image = new Image();
+        $form = $this->createForm(ImageType::class, $image);
+        
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()){
+            $file = $image->getName();
+            $fileName = md5(uniqid()).'.'.$file->guessExtension();
+            $file->move($this->getParameter('image_directory'), $fileName);
+            $image->setName($fileName);
+            
+            return $this->redirectToRoute('index');
+        }
+        
+        return $this->render('index.html.twig', array(
+            'form' => $form->createView(),
+        ));
     }
  
     /**
