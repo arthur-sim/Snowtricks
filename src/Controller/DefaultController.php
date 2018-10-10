594c4720a715384d9401caa1b78ae204a19b13f0
Unknown <arthur.simonian@outlook.fr>
2018-10-10 18:10:51 +0200

    /css:

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -5,15 +5,16 @@ namespace App\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
- 
+use App\Repository\TricksRepository;
+
 class DefaultController extends Controller
 {
     /**
-     * @Route("/", name="index")
+     * @Route("/", name="index", methods="GET")
      */
-    public function index()
+    public function index(TricksRepository $tricksRepository): Response
     {
-        return $this->render('index.html.twig');
+        return $this->render('index.html.twig', ['tricks' => $tricksRepository->findAll()]);
     }
  
     /**
