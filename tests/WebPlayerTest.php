01691b3380ffb2fcc560bf93439e386e3490c5d3
Unknown <arthur.simonian@outlook.fr>
2019-01-10 17:58:10 +0100

    test regex

---
+++ tests/WebPlayerTest.php
@@ -0,0 +1,26 @@
+<?php
+ 
+namespace App\Tests\Service;
+use App\Service\WebPlayer;
+use PHPUnit\Framework\TestCase;
+ 
+class WebPlayerTest extends TestCase
+{
+    private $webPlayer;
+ 
+    /**
+     * Sets up the fixture, for example, open a network connection.
+     * This method is called before a test is executed.
+     */
+    protected function setUp()
+    {
+        $this->webPlayer = new WebPlayer();
+    }
+ 
+    public function testYoutubeLink()
+    {
+        $this->assertEquals('https://www.youtube.com/embed/ktkjUjcZid0', $this->webPlayer->convertToIFrameUrl('https://www.youtube.com/watch?v=ktkjUjcZid0'));
+        $this->assertEquals('https://www.youtube.com/embed/ktkjUjcZid0', $this->webPlayer->convertToIFrameUrl('https://youtu.be/ktkjUjcZid0'));
+//        $this->assertEquals('https://www.youtube.com/embed/ktkjUjcZid0', $this->webPlayer->convertToIFrameUrl('https://www.youtube.com/embed/ktkjUjcZid0X'));
+    }
+}
