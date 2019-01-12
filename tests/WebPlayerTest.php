4d5422838fedc0c866857ee1e391efda51a81d0d
Unknown <arthur.simonian@outlook.fr>
2019-01-12 12:26:03 +0100

    update regex test
    daylimotion

--- tests/WebPlayerTest.php
+++ tests/WebPlayerTest.php
@@ -21,6 +21,13 @@ class WebPlayerTest extends TestCase
     {
         $this->assertEquals('https://www.youtube.com/embed/ktkjUjcZid0', $this->webPlayer->convertToIFrameUrl('https://www.youtube.com/watch?v=ktkjUjcZid0'));
         $this->assertEquals('https://www.youtube.com/embed/ktkjUjcZid0', $this->webPlayer->convertToIFrameUrl('https://youtu.be/ktkjUjcZid0'));
-//        $this->assertEquals('https://www.youtube.com/embed/ktkjUjcZid0', $this->webPlayer->convertToIFrameUrl('https://www.youtube.com/embed/ktkjUjcZid0X'));
+        $this->assertEquals('https://www.youtube.com/embed/ktkjUjcZid0', $this->webPlayer->convertToIFrameUrl('https://www.youtube.com/embed/ktkjUjcZid0'));
     }
+    public function testDailymotionLink()
+    {
+        $this->assertEquals('https://www.dailymotion.com/embed/video/ktkjUjcZid0', $this->webPlayer->convertToIFrameUrl('https://www.dailymotion.com/video/ktkjUjcZid0'));
+        $this->assertEquals('https://www.dailymotion.com/embed/video/ktkjUjcZid0', $this->webPlayer->convertToIFrameUrl('https://dai.ly/ktkjUjcZid0'));
+        $this->assertEquals('https://www.dailymotion.com/embed/video/ktkjUjcZid0', $this->webPlayer->convertToIFrameUrl('https://www.dailymotion.com/embed/video/ktkjUjcZid0'));
+    }
+    
 }
